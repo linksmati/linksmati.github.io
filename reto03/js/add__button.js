84e08505a80e90e4df0fae38d05022234587d1d2
Matías Herrera Posse <matiasherreraposse0@gmail.com>
2023-02-24 00:55:36 -0300

    listo el pop up y se empezo con el js

--- reto03/js/add__button.js
+++ reto03/js/add__button.js
@@ -1,4 +1,12 @@
 const button = document.getElementById('add_button')
+const pop_up = document.getElementById('pop_up')
+const main__fondo = document.getElementById('main__fondo')
+
 button.addEventListener('click', ()=>{
-  
+    pop_up.style.display = 'block'
+    main__fondo.style.display = 'none'
 })
+
+function popUp (){
+    if(pop_up == )
+}

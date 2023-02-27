f871cd815968ffaa9027555ba047b75ba1328d52
Matías Herrera Posse <matiasherreraposse0@gmail.com>
2023-02-27 20:09:08 -0300

    js

--- reto03/js/add__button.js
+++ reto03/js/add__button.js
@@ -1,12 +1,21 @@
 const button = document.getElementById('add_button')
-const pop_up = document.getElementById('pop_up')
-const main__fondo = document.getElementById('main__fondo')
+const pop_up = document.querySelector('.pop_up')
+const main = document.querySelector('.main')
 
 button.addEventListener('click', ()=>{
-    pop_up.style.display = 'block'
-    main__fondo.style.display = 'none'
+    pop_up.classList.add('mostrar')
 })
 
-function popUp (){
-    if(pop_up == )
-}
+
+
+
+
+// function popUp (){
+//     if(pop_up.style.opacity === 0){
+//         pop_up.style.opacity = 1;
+//         main__fondo.style.opacity = 0
+//     }else if(pop_up.style.opacity === 1){
+//         pop_up.style.opacity = 0;
+//         main__fondo.style.opacity = 1;
+//     }
+// }

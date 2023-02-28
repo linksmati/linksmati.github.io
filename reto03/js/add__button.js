789fad96459769001a34c5daa8628d9dc1ea5f80
Matías Herrera Posse <matiasherreraposse0@gmail.com>
2023-02-28 15:50:58 -0300

    problema del pushup arreglado

--- reto03/js/add__button.js
+++ reto03/js/add__button.js
@@ -3,8 +3,13 @@ const pop_up = document.querySelector('.pop_up')
 const fondo = document.querySelector('.fondo')
 
 button.addEventListener('click', function(){
+    if(pop_up.classList !== 'mostrar'){
     pop_up.classList.add('mostrar');
     fondo.classList.add('esconder')
+    } else if(pop_up.classList === 'mostrar'){
+        pop_up.classList.add('esconder');
+        fondo.classList.add('mostrar')
+    }
 })
 
 

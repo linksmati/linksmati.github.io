8ffcbbedeb049df4e6220f0d33f6e944c81e5773
Matías Herrera Posse <matiasherreraposse0@gmail.com>
2023-03-10 02:09:24 -0300

    jsjsjjsj

--- reto03/js/main.js
+++ reto03/js/main.js
@@ -1,41 +1,26 @@
 const button = document.getElementById('add_button')
 const pop_up = document.querySelector('.pop_up')
 const fondo = document.querySelector('.fondo')
+const contenedorCards = document.querySelector('.contenedorCards');
+let tareas = function (nombreTarea, descripcionTarea, tipotarea) {
+    contenedorCards.innerHTML += 
+    `<div class="card">
+        <div class="tipodetarea">
+            <h2>${tipotarea}</h2>
+        </div>
+        <div class="card__contenido">
+            <h3>${nombreTarea}</h3>
+            <p>${descripcionTarea}</p>
+        </div>
+    </div> `
+}
 
 // POP UP
 button.addEventListener('click', function(){
-    if(pop_up.classList.value !== 'pop_up mostrar'){
     pop_up.classList.add('mostrar');
-    fondo.classList.add('esconder')
-    } else{
-        pop_up.classList.remove('mostrar');
-        fondo.classList.remove('esconder')
-    }
+    fondo.classList.add('esconder');
+    contenedorCards.classList.add('esconder');
 })
-
-// const cancelarTarea = document.querySelector('#popupCancelar').addEventListener('click', ()=>{
-//     pop_up.classList.remove('mostrar');
-// })
-
-
-
-// CREACION DEL HTML
-const contenedorCards = document.querySelector('.contenedorCards');
-const tareas = (nombreTarea, descripcionTarea, tipotarea) => {
-    contenedorCards.innerHTML += `
-    <div class="card">
-    <div class="tipodetarea">
-        <h2>${tipotarea}</h2>
-    </div>
-    <div class="card__contenido">
-        <h3>${nombreTarea}</h3>
-        <p>${descripcionTarea}</p>
-    </div>
-</div>
-    
-    `
-}
-
 
 //FORMULARIO
 let formulario = document.querySelector('#formulario')
@@ -45,10 +30,34 @@ formulario.addEventListener('submit', (e) =>{
     let nombreTarea = e.target.nombreTarea.value;
     let tipoTarea = document.querySelector('#seleccionarTarea').value
     let descripcionTarea = e.target.descripcionTarea.value;
-
     tareas(nombreTarea, descripcionTarea, tipoTarea)
-
 })
+
+
+
+const agregarTarea = document.querySelector('#popupAgregar').addEventListener('click', ()=>{
+    pop_up.classList.remove('mostrar');
+    contenedorCards.classList.remove('esconder');
+    contenedorCards.classList.add('haytareas');
+})
+
+const cancelarTarea = document.querySelector('#popupCancelar').addEventListener('click', ()=>{
+    pop_up.classList.remove('mostrar');
+    if(contenedorCards.classList.value === 'contenedorCards haytareas esconder'){
+        contenedorCards.classList.remove('esconder');
+        fondo.classList.add('esconder');
+    }else{
+        fondo.classList.remove('esconder');
+        contenedorCards.classList.add('esconder');
+    }
+    
+})
+
+// CREACION DEL HTML
+
+
+
+
 
 
 

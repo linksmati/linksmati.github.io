38d6120001bca869f4215e55c682aad3c35011e6
Matías Herrera Posse <matiasherreraposse0@gmail.com>
2023-03-10 20:24:02 -0300

    jsjsjjs

--- reto03/js/main.js
+++ reto03/js/main.js
@@ -1,19 +1,24 @@
 const button = document.getElementById('add_button')
 const pop_up = document.querySelector('.pop_up')
 const fondo = document.querySelector('.fondo')
+// CREACION DEL HTML
 const contenedorCards = document.querySelector('.contenedorCards');
-let tareas = function (nombreTarea, descripcionTarea, tipotarea) {
+let tareas = function (prioridad, imagenTarea, nombreTarea) {
     contenedorCards.innerHTML += 
-    `<div class="card">
+    `<div class="card ${prioridad}">
         <div class="tipodetarea">
-            <h2>${tipotarea}</h2>
+            <img src="${imagenTarea}" alt="">
         </div>
-        <div class="card__contenido">
-            <h3>${nombreTarea}</h3>
-            <p>${descripcionTarea}</p>
+        <div class="nombredelatarea">
+            <p>${nombreTarea}<p>
         </div>
-    </div> `
+        <div class="completada">
+            <input class="listoBorrar" type="checkbox">
+            <button><img src="/reto03/assets/icons/delete.svg" alt=""></button>
+        </div>
+    </div>`
 }
+
 
 // POP UP
 button.addEventListener('click', function(){
@@ -21,19 +26,6 @@ button.addEventListener('click', function(){
     fondo.classList.add('esconder');
     contenedorCards.classList.add('esconder');
 })
-
-//FORMULARIO
-let formulario = document.querySelector('#formulario')
-
-formulario.addEventListener('submit', (e) =>{
-    e.preventDefault()
-    let nombreTarea = e.target.nombreTarea.value;
-    let tipoTarea = document.querySelector('#seleccionarTarea').value
-    let descripcionTarea = e.target.descripcionTarea.value;
-    tareas(nombreTarea, descripcionTarea, tipoTarea)
-})
-
-
 
 const agregarTarea = document.querySelector('#popupAgregar').addEventListener('click', ()=>{
     pop_up.classList.remove('mostrar');
@@ -53,7 +45,43 @@ const cancelarTarea = document.querySelector('#popupCancelar').addEventListener(
     
 })
 
-// CREACION DEL HTML
+//FORMULARIO
+let formulario = document.querySelector('#formulario')
+
+formulario.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    let nombreTarea = e.target.nombreTarea.value;
+    let tipoTarea = document.querySelector('#seleccionarTarea').value;
+
+    // IMAGEN
+    let imagenTarea = ''
+    if(tipoTarea === "Trabajo"){
+        imagenTarea = '/reto03/assets/icons/work_task.svg';
+    }else if(tipoTarea === "Personal"){ 
+        imagenTarea = '/reto03/assets/icons/personal_task.svg';
+    }else if(tipoTarea === "Domestica"){
+        imagenTarea = '/reto03/assets/icons/house_task.svg';
+    }
+    console.log(imagenTarea);
+
+    // PRIORIDAD
+    let prioridad= ''
+    let checkbox = document.querySelectorAll('#checkbox');
+    if(checkbox[0].checked === true && checkbox[1].checked === false && checkbox[2].checked === false){   
+        prioridad ='prioridad1'
+    } else if(checkbox[0].checked === false && checkbox[1].checked === true && checkbox[2].checked === false){
+        prioridad ='prioridad2'
+    } else if(checkbox[0].checked === false && checkbox[1].checked === false && checkbox[2].checked === true){
+        prioridad ='prioridad3'
+    }
+    console.log(checkbox[0].checked);   
+
+    tareas(prioridad, imagenTarea, nombreTarea)
+})
+
+
+
+
 
 
 

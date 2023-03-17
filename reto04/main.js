895d201c9db8b717551d4b3cf6905e026a8b2da3
Matías Herrera Posse <matiasherreraposse0@gmail.com>
2023-03-17 03:57:15 -0300

    reto04

--- reto04/main.js
+++ reto04/main.js
@@ -1,12 +1,50 @@
 let player1 = document.querySelector('#player1');
 let player2 = document.querySelector('#player2');
+
+// Jugadores
+let jugador1 = [] 
+
+let jugador2 = [] 
+console.log(jugador1,jugador2);
+// Partidas
+const partida = []
+// Botones
 let play = document.querySelector('#play');
-const loading = document.querySelector('.pc__loading')
-const cartas = document.querySelector('.pc__contenedor')
-console.log();
-// juguemos.addEventListener('click', mostrarCartas())
+let x = document.querySelector('#x')
+// Selectores de paginas
+const paginaPrincipal = document.querySelector('.pp__contenedor');
+const loading = document.querySelector('.pc__loading');
+const cartas = document.querySelector('.pc__contenedor');
+const popUp =document.querySelector('.popup');
+const mostrarocultar = document.querySelector('.mostrarocultar');
+
+
+
+// Eventos
+play.addEventListener('click', mostrarCartas)
+x.addEventListener('click', mostrarPopUp)
+// Funciones
+function nombresJugadores(player1, player2){
+    jugador1.push({nombre: player1.value})
+    jugador2.push({nombre: player2.value})
+}
 
 function mostrarCartas (){
-    loading.classList.add('esconderPantalla')
-    cartas.classList.add('mostrarPantalla')
+    paginaPrincipal.style.display = "none";
+    loading.style.display = "flex";
+    setTimeout(()=>{
+        loading.style.display = "none";
+        cartas.style.display = "flex";
+    }, 300)
+    nombresJugadores(player1, player2)
 } 
+function mostrarPopUp(){
+    if(cartas.style.display == "flex" && popUp.style.display == "flex"){ 
+        popUp.style.display = "none"
+        mostrarocultar.style.display = "block"
+    }else if(cartas.style.display == "flex"){
+        mostrarocultar.style.display = "none"
+        popUp.style.display = "flex"
+        console.log(popUp);
+    } 
+}
